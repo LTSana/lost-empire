ff2ea1cac465649a9c24a00d6e5e1daa44b7a9a5
LT. Sana <sana.mngadi@gmail.com>
2021-02-14 11:10:49 +0200

    📄Added comment

--- App0/utils/load_shopify.py
+++ App0/utils/load_shopify.py
@@ -14,7 +14,10 @@ from shopify_app.decorators import shop_login_required
 def load_shopify(function):
 	@wraps(function)
 	def wrap(request, *args, **kwargs):
+
+		# Access the Shopify API
 		shop_url = f"https://{settings.SHOPIFY_API_KEY_STORE}:{settings.SHOPIFY_API_PASSWORD_STORE}@{settings.SHOPIFY_SHOP}.myshopify.com/admin/api/2021-01"
 		shopify.ShopifyResource.set_site(shop_url)
+		
 		return function(request, *args, **kwargs)
 	return wrap
